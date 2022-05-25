348b080623c699cd08873d49565c32ce65772147
Zmmhd <1638793949@qq.com>
2022-05-25 18:01:11 +0800

    debug

--- estate-management/src/main/java/com/heima/estatemanagement/controller/BuildingController.java
+++ estate-management/src/main/java/com/heima/estatemanagement/controller/BuildingController.java
@@ -12,53 +12,61 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
+ *
  * @Author: 朱明名
  * @Date: 2022/5/12
- * @Description: 
+ * @Description:
  */
 @RestController
 @RequestMapping("/building")
 public class BuildingController {
-    
+
     @Autowired
     private BuildingService buildingService;
 
     @RequestMapping("/find")
-    public Result find(){
+    public Result find() {
         List<Building> all = buildingService.findAll();
-        return new Result(false,200,"请求成功adasdasdas",all);
+        return new Result(false, 200, "请求成功adasdasdas", all);
     }
+
     @RequestMapping("/search")
-    public PageResult search(@RequestBody Map searchMap){
+    public PageResult search(@RequestBody Map searchMap) {
         Page<Building> page = buildingService.search(searchMap);
-        return new PageResult(true, StatusCode.OK, MessageConstant.COMMUNITY_SEARCH_SUCCESS,page.getResult(),page.getTotal());
+        return new PageResult(true, StatusCode.OK, MessageConstant.COMMUNITY_SEARCH_SUCCESS, page.getResult(), page.getTotal());
     }
 
     @RequestMapping("/add")
-    public Result add(@RequestBody Building building){
+    public Result add(@RequestBody Building building) {
+        Date date = new Date();
+        building.setCreateTime(new Timestamp(date.getTime()));
+        building.setUpdateTime(new Timestamp(date.getTime()));
         Boolean add = buildingService.add(building);
-        return new Result(true,StatusCode.OK,MessageConstant.COMMUNITY_ADD_SUCCESS);
+        return new Result(true, StatusCode.OK, MessageConstant.COMMUNITY_ADD_SUCCESS);
     }
+
     @RequestMapping("/findById")
-    public Result findById(Integer id){
+    public Result findById(Integer id) {
         Building community = buildingService.findById(id);
-        return new Result(true,StatusCode.OK,MessageConstant.COMMUNITY_FIND_BY_ID_SUCCESS,community);
+        return new Result(true, StatusCode.OK, MessageConstant.COMMUNITY_FIND_BY_ID_SUCCESS, community);
     }
 
     @RequestMapping("/update")
-    public Result update(@RequestBody Building building){
+    public Result update(@RequestBody Building building) {
         Boolean add = buildingService.update(building);
-        return new Result(true,StatusCode.OK,MessageConstant.COMMUNITY_UPDATE_SUCCESS);
+        return new Result(true, StatusCode.OK, MessageConstant.COMMUNITY_UPDATE_SUCCESS);
     }
 
     @RequestMapping("/del")
-    public Result del(@RequestBody List<Integer> ids){
+    public Result del(@RequestBody List<Integer> ids) {
         Boolean flag = buildingService.del(ids);
-        return new Result(true,StatusCode.OK,MessageConstant.COMMUNITY_DELETE_SUCCESS);
+        return new Result(true, StatusCode.OK, MessageConstant.COMMUNITY_DELETE_SUCCESS);
     }
 }
